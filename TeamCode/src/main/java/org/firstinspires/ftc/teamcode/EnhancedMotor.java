d0f00ae6360c9d519249e294ff2a89f8187a4d78
rfspringer <Soccer02!>
2018-10-09 13:07:51 -0500

    write function to set run mode of all motors in array

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EnhancedMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EnhancedMotor.java
@@ -1,4 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class EnhancedMotor {
+    public static void setRunModes(DcMotor[] dcMotors, DcMotor.RunMode runMode) {
+        for (DcMotor motor : dcMotors) {
+            motor.setMode(runMode);
+        }
+    }
 }
