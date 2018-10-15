e495c65f643c4d8c0cd996de03c76ad870fc13c2
rfspringer <Soccer02!>
2018-10-15 13:10:22 -0500

    create trajectory class for storage of equations

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Trajectory.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Trajectory {
+    private double timeToAcceleration;
+    private double timeToBeginDeceleration;
+    private double maxVelocity;
+    private double maxAcceleration;
+
+    public Trajectory() {
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrajectoryGenerator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrajectoryGenerator.java
@@ -1,16 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
 public class TrajectoryGenerator {
-    private Drive drive = Drive.getInstance();
     private double trajectoryLength;
 
-    public TrajectoryGenerator (double distance) {
+    public TrajectoryGenerator(double distance) {
         this.trajectoryLength = distance;
     }
 
-    private double distanceToAccelerateAndDecelerate (double maxAcceleration, double maxVelocity) {
+    private double distanceToAccelerateAndDecelerate (double acceleration, double maxVelocity) {
         // Rearranged kinematic equation v^2 = v_0^2 + 2ax
-        return Math.pow(maxVelocity, 2) / maxAcceleration;
+        return maxVelocity * maxVelocity / acceleration;
+    }
+
+    private double getCruisingDistance(double acceleration, double maxVelocity) {
+        return trajectoryLength - distanceToAccelerateAndDecelerate(acceleration, maxVelocity);
     }
 
     private boolean trajectoryIsLongerThanAccelerationAndDeceleration
