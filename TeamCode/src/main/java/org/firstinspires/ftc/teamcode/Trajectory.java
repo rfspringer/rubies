731406ca7e15a4d2e9ce8fd4615808a002be48b6
rfspringer <Soccer02!>
2018-10-16 12:28:34 -0500

    Create run method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Trajectory.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Trajectory.java
@@ -1,12 +1,40 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 public class Trajectory {
-    private double timeToAcceleration;
-    private double timeToBeginDeceleration;
+    // All in milliseconds
+    private double timeToChangeAcceleration;
+    private double timeOfAcceleration;
+    private double timeOfConstantVelocity;
+
     private double maxVelocity;
     private double maxAcceleration;
+    private double maxJerk;
 
     public Trajectory() {
+
+    }
+
+    public void run(ElapsedTime currentTime) {
+        if (currentTime.milliseconds() < timeOfAcceleration) {
+            accelerate();
+        } else if (timeOfAcceleration <= currentTime.milliseconds() && currentTime.milliseconds() < timeOfAcceleration + timeOfConstantVelocity) {
+            cruise();
+        } else if (timeOfAcceleration + timeOfConstantVelocity < currentTime.milliseconds()) {
+            decelerate();
+        }
+    }
+
+    public void accelerate() {
+
+    }
+
+    public void cruise() {
+
+    }
+
+    public void decelerate() {
 
     }
 }
