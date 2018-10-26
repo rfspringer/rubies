7b1971c546c8b197665515c34a2549b80210cd76
Unknown <rachel@gardner-springer.com>
2018-10-26 13:49:21 -0500

    add waitForStart() to lift encoder test so we can actually run it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/LiftEncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/LiftEncoderTest.java
@@ -2,13 +2,12 @@ package org.firstinspires.ftc.teamcode.Tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.HWMaps.Robot;
 
-@Autonomous(name="Lift Encoder Test", group="Tests")
+@TeleOp(name="Lift Encoder Test", group="Tests")
 public class LiftEncoderTest extends LinearOpMode {
-
-    // Declare OpMode members.
     private Robot robot = Robot.getInstance();
 
     @Override
@@ -17,6 +16,7 @@ public class LiftEncoderTest extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.addData("Instructions", "This code will display the encoder reading of the lift motor, which can be moved by hand to certain positions");
         telemetry.update();
+        waitForStart();
 
         while (opModeIsActive()) {
             telemetry.addData("Lift Encoder Pos", robot.lift.getEncoderCounts());
