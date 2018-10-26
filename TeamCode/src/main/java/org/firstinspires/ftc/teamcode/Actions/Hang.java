8a2bcc7cfd9d2354b33e0a9125e88acb289a1583
Rachel! <rachel@gardner-springer.com>
2018-10-26 14:31:54 -0500

    write a test for the hang action
    Signed-off-by: Rachel! <rachel@gardner-springer.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Actions/Hang.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Actions/Hang.java
@@ -2,11 +2,7 @@ package org.firstinspires.ftc.teamcode.Actions;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.teamcode.HWMaps.Robot;
-
 public class Hang extends Action {
-    private Robot robot = Robot.getInstance();
-
     @Override
     public void init() {
         robot.lift.getMotor().setMode(DcMotor.RunMode.RUN_TO_POSITION);
