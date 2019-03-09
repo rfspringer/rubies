b64b082050ef9d01992cca7d2985963f70aa16d6
Rachel! <rachel@gardner-springer.com>
2019-03-09 12:58:18 -0600

    implement junit framework

---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/PracticeTest.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+import org.junit.Test;
+import com.google.common.truth.Truth.assertThat;
+import static junit.framework.Assert.assertEquals;
+
+public class PracticeTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        int result = 2 + 2;
+        int expected = 4;
+        assertEquals(expected, result).isTr;
+    }
+}
